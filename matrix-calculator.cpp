1d96621916040d6d8c0ff225263f7d7fec6668bf
Jose <38721575+sharco-code@users.noreply.github.com>
2018-10-04 22:43:49 +0200

    0.3

--- matrix-calculator.cpp
+++ matrix-calculator.cpp
@@ -1,8 +1,68 @@
 #include <iostream>
 
+void *add(int a[][2], int b[][2]) {
+    int (*res)[2] = new int[2][2];
+
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            res[i][j] = a[i][j] + b[i][j];
+        }    
+    }
+
+    return res;
+}
+
+void *substract(int a[][2], int b[][2]) {
+    int (*res)[2] = new int[2][2];
+
+    std::cout << "comming soon.." << std::endl;
+
+    return res;
+}
+
+void *multiply(int a[][2], int b[][2]) {
+    int (*res)[2] = new int[2][2];
+
+    for (int i = 0; i < 2; i++) {
+        for(int j = 0; j < 2; j++) {
+            if(j==0) {
+                if(i==0) {
+                    res[i][j] = (a[i][j] * b[i][j]) + (a[i][j+1] * b[i+1][j]);
+                } else {
+                    res[i][j] = (a[i][j] * b[i-1][j]) + (a[i][j+1] * b[i][j]);
+                }   
+            } else {
+                if (i==1) {
+                    res[i][j] = (a[i][j-1] * b[i-1][j]) + (a[i][j] * b[i][j]);
+                } else {
+                    res[i][j] = (a[i][j] * b[i][j]) + (a[i+1][j] * b[i+1][j]);
+                }
+            }
+        }
+    }
+
+    return res;
+}
+
+void print(int result[][2], bool x) {
+    
+    //show results
+    if (x) {
+        std::cout << "\nResult " << std::endl;
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                std::cout << " " << result[i][j];
+            }    
+            std::cout << std::endl;
+        }
+    }
+
+}
+
 int main(void) {
 
-    int a[2][2], b[2][2], c[2][2];
+    int a[2][2], b[2][2];
+    int (*c)[2] = new int[2][2];
     char turn[2] = {'a', 'b'};
     int menu;
     bool is_operative;
@@ -34,46 +94,28 @@ int main(void) {
             std::cout << std::endl;
         }
     }
-    
     do {
         std::cout << "\nAdd(1), Substract(2), Multiply(3):";
         std::cin >> menu;
         switch (menu) {
             case 1:
                 is_operative = true;
-                    for (int i = 0; i < 2; i++) {
-                        for (int j = 0; j < 2; j++) {
-                            c[i][j] = a[i][j] + b[i][j];
-                        }    
-                    }
+                c = (int (*)[2]) add(a, b);
                 break;
             case 2:
                 is_operative = false;
-                std::cout << "coming soon..";
-                //coming soon
+                std::cout << "Comming soon.." << std::endl;
                 break;
             case 3:
-                is_operative = false;
-                std::cout << "coming soon..";
-                //coming soon
+                is_operative = true;
+                c = (int (*)[2]) multiply(a, b);
                 break;
             default:
                 std::cout << "Incorrect option";
         }
-    } while (menu != 1/* && menu != 2 && menu != 3 */);
+    } while (menu < 1 || menu > 3);
+    print(c, is_operative);
 
-    //show results
-    if (is_operative) {
-        std::cout << "\nResult " << std::endl;
-        for (int i = 0; i < 2; i++) {
-            for (int j = 0; j < 2; j++) {
-                std::cout << " " << c[i][j];
-            }    
-            std::cout << std::endl;
-        }
-    }
-    
-    
     return 0;
 }
 // by sharco 
